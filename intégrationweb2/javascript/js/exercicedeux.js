bc47e54f4b3da61cd0356986a4e85ed8da8b1f56
productionbarberousse <olivier.normandin@cegeptr.qc.ca>
2019-07-11 19:34:56 -0400

    javasciprt

---
+++ intégrationweb2/javascript/js/exercicedeux.js
@@ -0,0 +1,14 @@
+//Calculer la surface d’un rectangle dont la longueur et la largeur sont lues à l’
+// écran.
+
+var longueur;
+var largeur;
+var air;
+
+var longueur = Number(prompt("Écrivez la longueur"));
+var largeur = Number (prompt("Écrivez la largeur"));
+
+var air = longueur * largeur;
+
+
+alert("La superficie = "+air);
---
+++ intégrationweb2/javascript/js/exerciceun.js
@@ -0,0 +1,12 @@
+//lire un nombre et affiché carré
+
+var nombre;
+var carre;
+var cube;
+
+var nombre = Number(prompt("Entrez un chiffre s'il vous plait"));
+var carre = nombre * nombre
+var cube = nombre * nombre * nombre
+
+alert("Voici le résultat : " + carre);
+console.log("Voixi le résutlat : "+cube);
