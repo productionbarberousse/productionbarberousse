018a82c0cf0680dfadcc980502c4cc2c7c2e6e88
productionbarberousse <olivier.normandin@cegeptr.qc.ca>
2019-08-27 10:40:29 -0400

    flashtp2

--- intégrationweb2/flash/js/monscript.js
+++ intégrationweb2/flash/js/monscript.js
@@ -1,3 +1,4 @@
+$('#flashID').css("position" , "relative");
 var compteur;
 compteur = 0;
 
@@ -6,36 +7,37 @@ $("#boutonGauche").click(function() {
     $('#flashID').css("left" ,compteur);
 });
 
-$('#boutonDroite').click(function() {
+$("#boutonDroite").click(function() {
     compteur = compteur + 50;
-    $('#flashID').css("right" , compteur);
+    $('#flashID').css("left" ,compteur);
 
 });
+var image;
+var compteur2;
+compteur2 = 0;
+
+image = $('#flashID').attr('src');
 
 $( "#boutonChangerImage" ).click(function() {
-    var image ;
-    var compteur ;
 
-    compteur = 0;
-
-    image = $('#flashID').attr('src');
-
-    if((image === "img/flash.jpg") && (compteur < 1)){
+    if((image === "img/flash.jpg") && (compteur2 < 1)){
         $('#flashID') .attr( 'src' , 'img/flash2.jpg');
+
     }
     else {
         $('#flashID') .attr( 'src' , 'img/flash.jpg');
+
     }
-    compteur = compteur
+    compteur2 = compteur2 + 1;
 
 });
 
 $( "#boutonEnleverImage" ).click(function() {
-    $('#flashID').fadeOut();
+    $('img').fadeOut();
 
 });
 $( "#boutonAjouterImage" ).click(function() {
-    $('#flashID').fadeIn();
+    $('img').fadeIn();
 
 });
 
@@ -43,9 +45,66 @@ $( "#boutonAjouterAttribut" ).click(function() {
     $('#flashID').attr("alt" , "photo de flash");
 
 });
+var compteur3;
+compteur3 = 0;
 $( "#boutonAjouterTitre" ).click(function() {
-    $('#flashID').before("<h1>FLASH JQUERY</h1>");
+
+    if (compteur3 < 1)
+        $('#flashID').before("<h1>FLASH JQUERY</h1>");
+        compteur3 = compteur3 +1 ;
 
 });
+$( "#boutonDupliquer" ).click(function() {
+    $('#flashID').after("<img id=\"flashID\" class=\"flashClass\" src=\"img/flash.jpg\" width=\"200px\"/>");
+
+});
+
+$( "#boutonRalentir" ).click(function() {
+    $('#flashID').remove();
+
+});
+
+
+$( "#boutonTeleport" ).click(function () {
+    if ($('#flashID').attr('src') === "img/flash.jpg"){
+        $('#flashID').removeAttr('src');
+        $('#teleport').attr('src' , 'img/flash.jpg');
+    }
+    else {
+        $('#flashID') .attr( 'src' , 'img/flash.jpg');
+        $('#teleport') .removeAttr('src');
+    }
+
+});
+
+
+
+
+
+$("#boutonVitesseSupreme").click(function() {
+    var supremevitesse = setInterval(disparaitreFlash, 500);
+
+});
+
+function disparaitreFlash() {
+    $('#flashID').toggle();
+   
+}
+$("#boutonStoptoute").click(function(){
+    $("flashID").clearInterval(supremevitesse);
+
+});
+
+
+
+
+
+
+$("#boutonNuke").click(function () {
+    $('body').html("<img class=\"flashClass\" src=\"img/bombe.jpg\"/>")
+
+});
+
+
 
 
